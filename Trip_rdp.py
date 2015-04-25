8969f8a8f746eea2821f7ef436668f87bb254644
mkery <mkery@wellesley.edu>
2015-04-25 14:23:15 -0400

    moved updated rdp code into rdp_trip.py

--- Trip_rdp.py
+++ Trip_rdp.py
@@ -2,68 +2,13 @@ import matplotlib.pyplot as pyplot
 import numpy as np
 import os
 import sys
-import rdp_trip
+from rdp_trip import *
 import math
 import Pmf
 
 
 def distance(x0, y0, x1, y1):
 	return math.sqrt((x1-x0)**2 + (y1-y0)**2)
-
-"""def distance_point(pt1, pt2):
-	return math.sqrt((pt2[0]-pt1[0])**2 + (pt2[1]-pt2[0])**2)
-
-def dist_point_to_line(pt, ln_a, ln_b):
-	l2 = distance_point(ln_a, ln_b)
-	if l2 == 0:
-		return distance_point(pt, ln_a)
-
-	t = ((pt[0] - ln_a[0]) * (ln_b[0] - ln_a[0]) + (pt[1] - ln_a[1]) * (ln_b[1] - ln_a[1])) / l2
-	if t < 0:
-		return distance_point(pt, ln_a)
-	if t > 1:
-		return distance_point(pt, ln_b)
-	return distance_point(pt, (ln_a[0] + t * (ln_b[0] - ln_a[0]),   ln_a[1] + t * (ln_b[1] - ln_a[1])))
-"""
-
-def pldist(x0, x1, x2):
-    """
-    Calculates the distance from the point ``x0`` to the line given
-    by the points ``x1`` and ``x2``.
-    :param x0: a point
-    :type x0: a 2x1 numpy array
-    :param x1: a point of the line
-    :type x1: 2x1 numpy array
-    :param x2: another point of the line
-    :type x2: 2x1 numpy array
-    """
-
-    if x1[0] == x2[0]:
-        return np.abs(x0[0] - x1[0])
-
-    return np.divide(np.linalg.norm(np.linalg.det([x2 - x1, x1 - x0])),
-                     np.linalg.norm(x2 - x1))
-
-
-def rdp_simplify(trip, epsilon):
-	# find the point with the maximum distance
-	dmax = 0
-	index = 0
-	for i in range(1, trip.shape[0]): #every point but first and last
-		d = pldist( np.array([trip[i][0], trip[i][1]]),
-					np.array([trip[0][0], trip[0][1]]),
-					np.array([trip[-1][0], trip[-1][1]]) )
-		if (d > dmax):
-			index = i
-			dmax = d
-	# If max distance is greater than epsilon, recursively simplify
-	if (dmax > epsilon):
-		#build the result list
-		res1 = rdp_simplify(trip[:index+1], epsilon)
-		res2 = rdp_simplify(trip[index:], epsilon)
-		return np.vstack((res1, res2))
-	else:
-		return np.vstack((trip[0],trip[-1]))
 
 
 def findSpeed_Dist(trip):
@@ -109,9 +54,9 @@ class Trip(object):
 	def __init__(self, filename):
 
 		#read in trip from file
-	 	self.tripPath = np.genfromtxt(filename, delimiter=',', skip_header=1)
+	 	tripPath = np.genfromtxt(filename, delimiter=',', skip_header=1)
 	 	#add a column for time in seconds (so if we chop data, still have timepoints)
-	 	#self.tripPath = np.append(tripPath, np.arange(tripPath.shape[0]).reshape(tripPath.shape[0],1),1)
+	 	self.tripPath = np.append(tripPath, np.arange(tripPath.shape[0]).reshape(tripPath.shape[0],1),1)
 	 	
 	 	self.rdp = rdp_simplify(self.tripPath, epsilon = 0.75)
 	 	print "DONE rdp_simplify!"
@@ -217,6 +162,6 @@ class Trip(object):
 trip_test = Trip(sys.argv[1])
 trip_test.plotTrip()
 
-#print trip_test.advSpeed
+print trip_test.rdp
 
 
--- rdp_trip.py
+++ rdp_trip.py
@@ -1,23 +1,14 @@
-"""
-rdp
-~~~
-Pure Python implementation of the Ramer-Douglas-Peucker algorithm.
-:copyright: (c) 2014 Fabian Hirschmann <fabian@hirschmann.email>
-:license: MIT, see LICENSE.txt for more details.
-"""
 import numpy as np
+"""
+edited 4/25 to fit trip default numpy format
+If you import a trip and then add a 3rd column to the trip that is time,
+when this runs the time field is kept... a bit hacky but works.
 
-def pldist(x0, x1, x2):
-    """
-    Calculates the distance from the point ``x0`` to the line given
-    by the points ``x1`` and ``x2``.
-    :param x0: a point
-    :type x0: a 2x1 numpy array
-    :param x1: a point of the line
-    :type x1: 2x1 numpy array
-    :param x2: another point of the line
-    :type x2: 2x1 numpy array
-    """
+"""
+
+def dist_point_to_line(x0, x1, x2):
+    """ Calculates the distance from the point ``x0`` to the line given
+    by the points ``x1`` and ``x2``, all numpy arrays """
 
     if x1[0] == x2[0]:
         return np.abs(x0[0] - x1[0])
@@ -26,58 +17,22 @@ def pldist(x0, x1, x2):
                      np.linalg.norm(x2 - x1))
 
 
-def _rdp(M, epsilon, dist):
-    """
-    Simplifies a given array of points.
-    :param M: an array
-    :type M: Nx2 numpy array
-    :param epsilon: epsilon in the rdp algorithm
-    :type epsilon: float
-    :param dist: distance function
-    :type dist: function with signature ``f(x1, x2, x3)``
-    """
-    dmax = 0.0
-    index = -1
-
-    for i in range(1, M.shape[0]):
-        d = dist(M[i], M[0], M[-1])
-
-        if d > dmax:
+def rdp_simplify(trip, epsilon):
+    # find the point with the maximum distance
+    dmax = 0
+    index = 0
+    for i in range(1, trip.shape[0]): #every point but first and last
+        d = dist_point_to_line( np.array([trip[i][0], trip[i][1]]),
+                    np.array([trip[0][0], trip[0][1]]),
+                    np.array([trip[-1][0], trip[-1][1]]) )
+        if (d > dmax):
             index = i
             dmax = d
-
-    if dmax > epsilon:
-        r1 = _rdp(M[:index + 1], epsilon, dist)
-        r2 = _rdp(M[index:], epsilon, dist)
-
-        return np.vstack((r1[:-1], r2))
+    # If max distance is greater than epsilon, recursively simplify
+    if (dmax > epsilon):
+        #build the result list
+        res1 = rdp_simplify(trip[:index+1], epsilon)
+        res2 = rdp_simplify(trip[index:], epsilon)
+        return np.vstack((res1[:-1], res2)) #not sure why [:-1] works, but it keeps duplicates from happening
     else:
-        return np.vstack((M[0], M[-1]))
-
-
-def _rdp_nn(seq, epsilon, dist):
-    """
-    Simplifies a given array of points.
-    :param seq: a series of points
-    :type seq: sequence of 2-tuples
-    :param epsilon: epsilon in the rdp algorithm
-    :type epsilon: float
-    :param dist: distance function
-    :type dist: function with signature ``f(x1, x2, x3)``
-    """
-    return rdp(np.array(seq), epsilon, dist).tolist()
-
-
-def rdp(M, epsilon=0, dist=pldist):
-    """
-    Simplifies a given array of points.
-    :param M: a series of points
-    :type M: either a Nx2 numpy array or sequence of 2-tuples
-    :param epsilon: epsilon in the rdp algorithm
-    :type epsilon: float
-    :param dist: distance function
-    :type dist: function with signature ``f(x1, x2, x3)``
-    """
-    if "numpy" in str(type(M)):
-        return _rdp(M, epsilon, dist)
-    return _rdp_nn(M, epsilon, dist)
+        return np.vstack((trip[0],trip[-1]))
